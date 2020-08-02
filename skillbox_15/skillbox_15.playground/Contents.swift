2ffd7e8057291eabda2af961b6ded7d1e2590283
maxibello <saintwater@ya.ru>
2020-08-02 17:31:41 +0300

    review

--- skillbox_15/skillbox_15.playground/Contents.swift
+++ skillbox_15/skillbox_15.playground/Contents.swift
@@ -3,9 +3,42 @@ import UIKit
 // Напишите своими словами, что такое pure function
 //Это функция, которая не создает побочных эффектов для экосистемы, в которой она запущена и которая не зависит от каких-либо внешних состояний. Она всегда возвращает один и тот же результат для одного и того же набора входных параметров, независимо от того, когда и сколько раз была вызвана.
 
+//Реализуем любой алгоритм сортировки, отличный от того, который реализовали во втором модуле?
+//Там использовали сортировку пузырьком.
+//Здесь можно любую другую.
+
+extension Array where Element: Comparable {
+func insertionSort() -> Array<Element> {
+       
+       guard self.count > 1 else {
+           return self
+       }
+       
+       var output: Array<Element> = self
+       
+       for primaryindex in 0..<output.count {
+           
+           let key = output[primaryindex]
+           var secondaryindex = primaryindex
+           
+           while secondaryindex > -1 {
+               if key < output[secondaryindex] {
+                   output.remove(at: secondaryindex + 1)
+                   output.insert(key, at: secondaryindex)
+               }
+               
+               secondaryindex -= 1
+           }
+       }
+       
+       return output
+   }
+}
+
 // Отсортируйте массив чисел по возрастанию используя функцию sorted
 let intArr = [23, 100, 344, 0, -10, 43]
 let newArr = intArr.sorted()
+let sortedByInsertion = intArr.insertionSort()
 
 // Переведите массив числе в массив строк с помощью функции map
 let stringArr = newArr.map { String($0) }
@@ -25,12 +58,11 @@ func delayedForTwoSec(f:@escaping () -> ()) {
 }
 
 // Напишите функцию, которая принимает в себя две функции и возвращает функцию, которая при вызове выполнит первые две функции
-func delegateFunctions(f1: @escaping () -> (), f2: @escaping () -> ()) -> () -> () {
-    let f = {
+func delegateFunctions(f1: @escaping () -> Void, f2: @escaping () -> Void) -> () -> Void {
+    return {
         f1()
         f2()
     }
-    return f
 }
 
 //delegateFunctions(f1: { print("f1") }, f2: { print("f2") })()
@@ -43,6 +75,14 @@ func setupSort(for arr: [Int], f: (Int, Int) -> Bool) -> [Int] {
 
 // Напишите своими словами что такое infix, suffix, prefix операторы.
 //Операторы позволяют добавлять типам кастомное поведение, например:
-// - infix, добавлять такие операторы как +-
+// - infix, определять свою логику для бинарных операторов (+-)
 // - prefix, определить свою логику для унарного минуса
-// - postfix, определить свою логику операторы, который следует за операндом (b!)
+// - postfix, определить свою логику оператора, который следует за операндом (b!)
+
+
+//Реализуйте функцию join(_ array: [String], separator: String) -> String с помощью reduce.
+//Сепаратор не должен добавляться к последнему элементу.
+
+func join(_ array: [String], separator: String) -> String {
+    return String(array.reduce("", { $0 + $1 + separator}).dropLast(separator.count))
+}
