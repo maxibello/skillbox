42ea7f15aa56628d71f04264a05f8cc5b4e5d9a2
maxibello <saintwater@ya.ru>
2020-10-02 19:55:57 +0300

    RootView

--- s02e04_mvc/s02e04_mvc/Controller/CategoryVC.swift
+++ s02e04_mvc/s02e04_mvc/Controller/CategoryVC.swift
@@ -10,19 +10,11 @@ import UIKit
 
 class CategoryVC: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var rootView: RootView!
-    var categories: [Category] = []
-    var selectedCategory: Category?
     
     override func viewDidLoad() {
-        tableView.delegate = self
-        tableView.dataSource = self
         
-        tableView.tableFooterView = UIView()
-        tableView.allowsSelection = false
-        
-        guard categories.count == 0 else { return }
+        guard rootView.categories.count == 0 else { return }
         
         rootView.loader.startAnimating()
         BlackStarApiService.loadCategories() { [weak self] result in
@@ -35,32 +27,11 @@ class CategoryVC: UIViewController {
                     .filter { $0.subcategories?.count ?? 0 > 0 }
                     .sorted(by: { Int($0.sortOrder) ?? Int.max < Int($1.sortOrder) ?? Int.max
                     })
-                self.categories = sortedCategories
-//                print(self.categories)
-                self.tableView.reloadData()
+                self.rootView.categories = sortedCategories
+                self.rootView.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
         }
-    }
-    
-
-}
-
-extension CategoryVC: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        categories.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as! CategoryViewCell
-        cell.configure(with: categories[indexPath.row])
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        selectedCategory = categories[indexPath.row]
-        return indexPath
     }
 }
--- s02e04_mvc/s02e04_mvc/View/RootView.swift
+++ s02e04_mvc/s02e04_mvc/View/RootView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class RootView: UIView {
     
+    var categories: [Category] = []
+    
     lazy var loader: UIActivityIndicatorView = {
         let loader = UIActivityIndicatorView()
         loader.translatesAutoresizingMaskIntoConstraints = false
@@ -19,18 +21,56 @@ class RootView: UIView {
         return loader
     }()
     
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.tableFooterView = UIView()
+        tableView.allowsSelection = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "CategoryCell", bundle: nil), forCellReuseIdentifier: "CategoryCell")
+
+        return tableView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(loader)
-        
-        NSLayoutConstraint.activate([
-            loader.centerXAnchor.constraint(equalTo: centerXAnchor),
-            loader.centerYAnchor.constraint(equalTo: centerYAnchor)
-        ])
+        commonInit()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        commonInit()
     }
+    
+    private func commonInit() {
+        addSubview(tableView)
+        addSubview(loader)
+        
+        NSLayoutConstraint.activate([
+            loader.centerXAnchor.constraint(equalTo: centerXAnchor),
+            loader.centerYAnchor.constraint(equalTo: centerYAnchor),
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+}
 
+extension RootView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as! CategoryViewCell
+        cell.configure(with: categories[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
 }
