95f24c86c4378d6abddbd22a78cf803cf55e7036
maxibello <saintwater@ya.ru>
2021-06-10 11:26:30 +0300

    all tasks

--- s02e12_coreml/ClassifyingImagesWithVisionAndCoreML/Vision+ML Example/ImageClassificationViewController.swift
+++ s02e12_coreml/ClassifyingImagesWithVisionAndCoreML/Vision+ML Example/ImageClassificationViewController.swift
@@ -22,13 +22,7 @@ class ImageClassificationViewController: UIViewController {
     /// - Tag: MLModelSetup
     lazy var classificationRequest: VNCoreMLRequest = {
         do {
-            /*
-             Use the Swift class `MobileNet` Core ML generates from the model.
-             To use a different Core ML classifier model, add it to the project
-             and replace `MobileNet` with that model's generated Swift class.
-             */
             let model = try VNCoreMLModel(for: SkillboxClassifier().model)
-            
             let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
                 self?.processClassifications(for: request, error: error)
             })
@@ -51,11 +45,6 @@ class ImageClassificationViewController: UIViewController {
             do {
                 try handler.perform([self.classificationRequest])
             } catch {
-                /*
-                 This handler catches general image processing errors. The `classificationRequest`'s
-                 completion handler `processClassifications(_:error:)` catches errors specific
-                 to processing that request.
-                 */
                 print("Failed to perform classification.\n\(error.localizedDescription)")
             }
         }
